0e9af107b2ad15189d0efa5244df99f9ef76fc33
hussam-ALJayyar <hosam.algayyar@gmail.com>
2023-05-16 04:45:15 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
+const middlewares = jsonServer.defaults({
+  static: "./build",
+});
+const PORT = process.env.PORT || 5000;
+server.use(middlewares);
+
+server.db = router.db;
+
+server.use(auth);
+server.use(router);
+server.listen(PORT, () => {
+  console.log("Server is running");
+});
